37289b33c2808832a29ebac40c8cd4e23f5d54cc
Alex Young <atyalexyoung@gmail.com>
2025-07-11 22:50:37 -0400

    Added initial angular starter code and framework.

--- FitnessApp.Shared/DTOs/Requests/CreateWorkoutRequest.cs
+++ FitnessApp.Shared/DTOs/Requests/CreateWorkoutRequest.cs
@@ -1,5 +1,8 @@
 ﻿namespace FitnessApp.Shared.DTOs.Requests
 {
+    /// <summary>
+    /// Class for the Request to create a workout that contains only the neccessary information to create the workout.
+    /// </summary>
     public class CreateWorkoutRequest
     {
         public string Name { get; set; } = string.Empty;
