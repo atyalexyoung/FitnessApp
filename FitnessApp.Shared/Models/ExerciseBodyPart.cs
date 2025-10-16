a0964d736b741ae3a0f26dc8185f06343088c2cd
atyalexyoung <atyalexyoung@gmail.com>
2025-10-16 15:32:02 -0400

    Fixing indenting on ExerciseBodyPart.cs and ExerciseTag.cs

--- FitnessApp.Shared/Models/ExerciseBodyPart.cs
+++ FitnessApp.Shared/Models/ExerciseBodyPart.cs
@@ -2,11 +2,11 @@ using FitnessApp.Shared.Enums;
 
 namespace FitnessApp.Shared.Models
 {
-public class ExerciseBodyPart
-{
-    public string ExerciseId { get; set; } = null!;
-    public Exercise Exercise { get; set; } = null!;
-    
-    public BodyParts.BodyPart BodyPart { get; set; }
-}
+    public class ExerciseBodyPart
+    {
+        public string ExerciseId { get; set; } = null!;
+        public Exercise Exercise { get; set; } = null!;
+        
+        public BodyParts.BodyPart BodyPart { get; set; }
+    }
 }
--- FitnessApp.Shared/Models/ExerciseTag.cs
+++ FitnessApp.Shared/Models/ExerciseTag.cs
@@ -2,11 +2,11 @@ using FitnessApp.Shared.Enums;
 
 namespace FitnessApp.Shared.Models
 {
-public class ExerciseTag
-{
-    public string ExerciseId { get; set; } = null!;
-    public Exercise Exercise { get; set; } = null!;
-    
-    public ExerciseTypes.ExerciseTypeTag Tag { get; set; }
-}
+    public class ExerciseTag
+    {
+        public string ExerciseId { get; set; } = null!;
+        public Exercise Exercise { get; set; } = null!;
+        
+        public ExerciseTypes.ExerciseTypeTag Tag { get; set; }
+    }
 }
